192ed76fc594a26ecfeb1c0d9a591c051ca92a60
Michael Williaks <manicdesigner@gmail.com>
2017-08-18 09:59:37 -0400

    add files from original source js_gary, borilerplate html5 boiler plate and some additions

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Sirin+Stencil" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/helpers.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <title>Garys Tool Selling Site</title>
+</head>
+<body>
+    <header class='container-fluid'>
+      <div class='row'>
+        <h1 class="col-xs-12 col-sm-6 tools">
+            Garys Tool List
+        </h1>
+     </header>
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 7/13/17
+ * Time: 5:47 AM
+ */
+
+
+require( '../inc/header.php');
+require('../inc/nav.php');
+?>
+
+
+
+
+<?php
+ require('../inc/footer.php');
+ ?>
