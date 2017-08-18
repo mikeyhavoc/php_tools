24b79461e0e2e166a203530caac2eef7ff552af5
Michael Williaks <manicdesigner@gmail.com>
2017-08-18 10:21:49 -0400

    add tool info p= details about listing for tools

--- public/index.php
+++ public/index.php
@@ -10,19 +10,33 @@
 require( '../inc/header.php');
 require('../inc/nav.php');
 ?>
-  <div class="container-fluid">
-  <div class="row">
-    <section class="col-xs-12 col-sm-6">
-      <h2>Info</h2>
-      <p>
-        Greetings, I am a retired body and fender man. I am selling off my tools now. I am located in the West Bradenton area. Sales are <em>local only</em> no shipping tools.
-      </p>
-    </section>
+    <div class="container-fluid">
+        <div class="row">
+            <section>
+              <article class="info col-xs-12 col-sm-3 col-sm-offset-3">
+                <h2>Info</h2>
+                <p>
+                  Greetings, I am a retired body and fender man. I am selling off my tools now. I am located in the West Bradenton area. Sales are local only, no shipping tools.
+                </p>
+              </article>
+              <article class="tool-info col-xs-12 col-sm-3">
+                  <h2>Tool Info</h2>
+                  <p>
+                    Important to note:
+                    <ul>
+                      <li>Generally <em>most</em> of these items there is only one of said item.</li>
+                      <li>if any questions about an Item please email</li>
+                      <li>listing will be updated asap after sales occur</li>
+                    </ul>
+                  </p>
+              </article>
 
- </div>
+            </section>
+
+       </div>
 
 
-  </div>
+    </div>
 
 
 <?php
