da024e0a7a9193ab6d40973187dafd0895c4d013
Michael Williaks <manicdesigner@gmail.com>
2017-08-18 10:00:30 -0400

    add info section

--- public/index.php
+++ public/index.php
@@ -10,8 +10,19 @@
 require( '../inc/header.php');
 require('../inc/nav.php');
 ?>
+  <div class="container-fluid">
+  <div class="row">
+    <section class="col-xs-12 col-sm-6">
+      <h2>Info</h2>
+      <p>
+        Greetings, I am a retired body and fender man. I am selling off my tools now. I am located in the West Bradenton area. Sales are <em>local only</em> no shipping tools.
+      </p>
+    </section>
+
+ </div>
 
 
+  </div>
 
 
 <?php
