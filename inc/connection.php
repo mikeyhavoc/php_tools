48ddc721829539ccb7833cb35e0acfa0f11365c1
Michael Williaks <manicdesigner@gmail.com>
2017-08-18 13:07:54 -0400

    add connection with connection info for database

---
+++ inc/connection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 8/18/17
+ * Time: 10:56 AM
+ */
+$user = 'mikey';
+$pass = 'DroidsLie#9';
+$base = 'gary';
+
+$con = mysqli_connect('localhost', $user, $pass, $base);
+
