27b8bf0d87829c29b55b2754fd2e5a383e859788
Michael Williaks <manicdesigner@gmail.com>
2017-08-22 10:45:53 -0400

    comment out dynamic root and add fixed root.

--- private/initialize.php
+++ private/initialize.php
@@ -12,7 +12,7 @@
 
 
 define("PRIVATE_PATH", dirname(__FILE__));  # private folder for private items.
-define("PROJECT_PATH", dirname(PRIVATE_PATH)); # pprivate folder for shared things
+define("PROJECT_PATH", dirname(PRIVATE_PATH)); # private folder for shared things
 define("PUBLIC_PATH", PROJECT_PATH . '/public'); # public folder
 define("SHARED_PATH", PRIVATE_PATH . '/shared'); # shared folder
 
@@ -21,14 +21,17 @@ define("SHARED_PATH", PRIVATE_PATH . '/shared'); # shared folder
 // * Use same document root as web server
 // * Can ser a hardcoded value:
 // e.g define("WWW_ROOT", '/~mike/globebank/public');
-// define("WWW_ROOT", '');
+
 // * Can be dynamically find everything in URL up to "/public"
 
-$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
-$doc_root = subtr(S_SERVER['SCRIPT_NAME'], 0, $public_end);
-define("WWW_ROOT", $doc_root);
+//$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+//$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+//define("WWW_ROOT", $doc_root);
 
+//ROOT of file.
+define("WWW_ROOT", '');
 
 
 
 require_once('functions.php');
+
