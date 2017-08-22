8876ecb79e092f9ceb1852920d18b66b05a0e98c
Michael Williaks <manicdesigner@gmail.com>
2017-08-22 15:11:52 -0400

    dummy data to test before adding database inputs.

---
+++ private/dum_data.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 8/21/17
+ * Time: 12:36 PM
+ */
+$tools = [
+    [
+        "item" => "g1",
+        "type" => "wrench",
+        "brand" => "blue-point",
+        "style" => [
+            "ratchet", "box/open"
+        ],
+        "piece" => "12",
+        "size" => "5/15 - 1 inch",
+        "retail" => "277.00",
+        "price" => "150.00",
+        "description" => "This was a shop set and is in great shape.",
+        "thumb" => "images/thumbs/wrench/g1_wrench.jpg",
+        "regular_img" => "images/med_img/wrench/g1_wrench.jpg"
+
+    ],
+    [
+        "item" => "a2",
+        "type" => "wrench",
+        "brand" => "gearwrench",
+        "style" => [
+            "jumbo", "ratchet", "open/box"
+        ],
+        "piece" => "8",
+        "size" => "20, 21, 22, 24, 25, 27, 30, 32mm",
+        "retail" => "375.00",
+        "price" => "150.00",
+        "description" => "this is an unused home set.",
+        "thumb" => "images/thumbs/wrench/a2_wrench.jpg",
+        "regular_img" => "images/med_img/wrench/a2_wrench.jpg"
+    ],
+    [
+        "item" => "g4",
+        "type" => "wrench",
+        "brand" => "blue-point",
+        "style" => [
+            "ratchet", "open/box", "jumbo"
+        ],
+        "piece" => "4",
+        "size" => "21, 22, 24, 25mm",
+        "retail" => "177.00",
+        "price" => "95.00",
+        "description" => "This is a home set that did not really ever leave the tool box. Life time tools, borem704",
+        "thumb" => "images/thumbs/wrench/g4_wrench.jpg",
+        "regular_img" => "images/med_img/wrench/g4_wrench.jpg"
+    ],
+    [
+        "item" => "ii5",
+        "type" => "wrench",
+        "brand" => "gearwrench",
+        "style" => [
+            "flex-head", "ratchet", "box/open"
+        ],
+        "piece" => "4",
+        "size" => "13/16 - 1 inch",
+        "retail" => "n/a",
+        "price" => "50.00",
+        "description" => "Little or no use for this home set, these are lifetime tools.",
+        "thumb" => "images/thumbs/wrench/ii5_wrench.jpg",
+        "regular_img" => "images/med_img/wrench/ii5_wrench.jpg"
+    ]
+
+
+];
