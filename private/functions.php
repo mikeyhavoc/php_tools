b1e901647e6586a28a372864414115143f0c43bf
Michael Williaks <manicdesigner@gmail.com>
2017-08-22 10:46:43 -0400

    function url_for() quick script to reach root.

--- private/functions.php
+++ private/functions.php
@@ -5,3 +5,11 @@
  * Date: 8/21/17
  * Time: 2:31 PM
  */
+
+function url_for($script_path) {
+    // adds leading '/' if not present.
+    if ($script_path[0] != '/') {
+        $script_path = '/' . $script_path;
+    }
+    return WWW_ROOT . $script_path;
+}
