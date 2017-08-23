4992944d92ed0a5f0ec77a24b82bf1aa6e3c2389
Michael Williaks <manicdesigner@gmail.com>
2017-08-23 12:33:56 -0400

    add modal window, preparing for building out with top 3 sections.

---
+++ private/shared/footer.php
@@ -0,0 +1,34 @@
+</main>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="myModal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button class="close" data-miss="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title"><?php echo $tool['title'] ?></h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    one fine day
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div><!-- / modal-content-->
+    </div><!--/ modal-dialog-->
+</div><!--/ modal-->
+
+
+
+
+
+<script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
