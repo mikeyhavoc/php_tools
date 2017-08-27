1e60d95aefa8c73337734ebbca79aa43e1bdca20
Michael Williaks <manicdesigner@gmail.com>
2017-08-27 17:18:13 -0400

    nav for small and extra small screens to hover or click and get instead of the buttons for extra space.

--- private/shared/nav.php
+++ private/shared/nav.php
@@ -1,5 +1,5 @@
 
-        <nav class="col-xs-12 col-sm-10">
+        <nav class=" hidden-sm hidden-xs col-xs-12 col-sm-10">
 
             <button type="button" class="btn btn-lg btn-default">
                 <a href="pages/wrenches.html">Wrenches</a>
@@ -91,5 +91,125 @@
             <a href="pages/blades.html">Blades</a>
           </button>
 
-            </ul>
+
         </nav>
+        <section class="visible-xs visible-sm">
+
+
+
+            </div>
+            <div class="dropdown">
+<!--                <button class="btn btn-primary navbar-toggle" type="button" date-toggle="dropdown" data-target="#miniMenu">-->
+<!--                    <span class="caret"></span>-->
+<!--                </button>-->
+                <ul class="nav navbar-nav" id="miniMenu">
+                     <li class="dropdown">
+                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="#miniMenu">
+                             <span class="caret"></span>
+                         </a>
+                     </li>
+                </ul>
+                <ul class="dropdown-menu" id="miniMenu">
+
+                <li>
+                    <a href="pages/wrenches.html">Wrenches</a>
+                </li>
+
+                <li>
+                    <a href="pages/files.html">Files</a>
+                </li>
+
+                <li>
+                    <a href="pages/pliers.html">Pliers</a>
+                </li>
+
+                <li>
+                    <a href="pages/bits.html">Bits</a>
+                </li>
+
+                <li>
+                    <a href="pages/air.html">Air Tools</a>
+                </li>
+
+                <li>
+                    <a href="pages/ratchets.html">Ratchet</a>
+                </li>
+
+                <li>
+                    <a href="pages/crimps.html">Crimps</a>
+                </li>
+
+                <li>
+                    <a href="pages/drills.html">Drills</a>
+                </li>
+
+                <li>
+                    <a href="pages/sockets.html">Sockets</a>
+                </li>
+
+                <li>
+                    <a href="pages/removers.html">Removers</a>
+                </li>
+
+                <li>
+                    <a href="pages/extensions.html">Extensions</a>
+                </li>
+
+                <li>
+                    <a href="pages/screwdrivers.html">Screwdrivers</a>
+                </li>
+
+                <li>
+                    <a href="pages/prybars.html">pry and pic bars</a>
+                </li>
+
+                <li>
+                    <a href="pages/cable.html">cables</a>
+                </li>
+
+                <li>
+                    <a href="pages/jacks.html">Jacks</a>
+                </li>
+
+                <li>
+                    <a href="pages/misc.html">Misc</a>
+                </li>
+
+                <li>
+                    <a href="pages/discs.html">cut/grind wheels</a>
+                </li>
+
+                <li>
+                    <a href="pages/clamps.html">Clamps</a>
+                </li>
+
+                <li>
+                    <a href="pages/chisels.html">chisels</a>
+                </li>
+
+                <li>
+                    <a href="pages/hammers">Hammers</a>
+                </li>
+
+                <li>
+                    <a href="pages/spoons.html">spoons</a>
+                </li>
+
+                <li>
+                    <a href="pages/visegrips.html">Vise Grips</a>
+                </li>
+
+                <li>
+                    <a href="pages/blades.html">Blades</a>
+                </li>
+            </ul>
+            </div>
+
+        </section>
+
+
+
+
+        </div>
+        </div>
+
