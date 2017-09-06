7396fe1f24742df5555e63b52c21f64b40fdc903
Michael Williaks <manicdesigner@gmail.com>
2017-09-06 08:14:25 -0400

    customized drop down to be more bootstrap styled to work properly.

--- private/shared/nav.php
+++ private/shared/nav.php
@@ -2,7 +2,7 @@
         <nav class=" hidden-sm hidden-xs col-xs-12 col-sm-10">
 
             <button type="button" class="btn btn-lg btn-default">
-                <a href="pages/wrenches.html">Wrenches</a>
+                <a href="<?php echo rawurlencode(url_for(WWW_ROOT . 'public/pages/wrenches.php')); ?>">Wrenches</a>
             </button>
             <button type="button" class="btn btn-lg btn-default">
                 <a href="pages/files.html">Files</a>
@@ -17,7 +17,7 @@
             </button>
 
             <button type="button" class="btn btn-lg btn-default">
-                <a href="pages/air.html">Air Tools</a>
+                <a href="<?php echo rawurlencode(url_for(WWW_ROOT . 'public/pages/air_tools.php')); ?>">Air Tools</a>
             </button>
 
             <button type="button" class="btn btn-lg btn-default">
@@ -33,7 +33,7 @@
             </button>
 
             <button type="button" class="btn btn-lg btn-default">
-                <a href="pages/sockets.html">Sockets</a>
+                <a href="<?php echo rawurlencode(url_for(WWW_ROOT . 'public/pages/sockets.php')); ?>">Sockets</a>
             </button>
 
             <button type="button" class="btn btn-lg btn-default">
@@ -102,107 +102,102 @@
 <!--                <button class="btn btn-primary navbar-toggle" type="button" date-toggle="dropdown" data-target="#miniMenu">-->
 <!--                    <span class="caret"></span>-->
 <!--                </button>-->
-                <ul class="nav navbar-nav col-xs-2" id="miniMenu">
-                     <li class="dropdown">
-                         <a href="#" class="dropdown-toggle btn btn-primary" data-toggle="dropdown" data-target="#miniMenu">
-                             <span class="caret"></span>
-                         </a>
-                     </li>
-                </ul>
-                <ul class="dropdown-menu" id="miniMenu">
+                <div class="dropdown col-xs-2">
+                    <button class="btn btn-default dropdown-toggle type=button" id="minimenu" data-toggle="dropdown"><span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu" aria-labeledby="minimenu">
+                        <li>
+                            <a href="<?php echo rawurlencode(url_for(WWW_ROOT . 'public/pages/wrenches.php')); ?>">Wrenches</a>
+                        </li>
 
-                <li>
-                    <a href="pages/wrenches.html">Wrenches</a>
-                </li>
+                        <li>
+                            <a href="pages/files.html">Files</a>
+                        </li>
 
-                <li>
-                    <a href="pages/files.html">Files</a>
-                </li>
+                        <li>
+                            <a href="pages/pliers.html">Pliers</a>
+                        </li>
 
-                <li>
-                    <a href="pages/pliers.html">Pliers</a>
-                </li>
+                        <li>
+                            <a href="pages/bits.html">Bits</a>
+                        </li>
 
-                <li>
-                    <a href="pages/bits.html">Bits</a>
-                </li>
+                        <li>
+                            <a href="<?php echo rawurlencode(url_for(WWW_ROOT . 'public/pages/air_tools.php')); ?>">Air Tools</a>
+                        </li>
 
-                <li>
-                    <a href="pages/air.html">Air Tools</a>
-                </li>
+                        <li>
+                            <a href="pages/ratchets.html">Ratchet</a>
+                        </li>
 
-                <li>
-                    <a href="pages/ratchets.html">Ratchet</a>
-                </li>
+                        <li>
+                            <a href="pages/crimps.html">Crimps</a>
+                        </li>
 
-                <li>
-                    <a href="pages/crimps.html">Crimps</a>
-                </li>
+                        <li>
+                            <a href="pages/drills.html">Drills</a>
+                        </li>
 
-                <li>
-                    <a href="pages/drills.html">Drills</a>
-                </li>
+                        <li>
+                            <a href="<?php echo rawurlencode(url_for(WWW_ROOT . 'public/pages/sockets.php')); ?>">Sockets</a>
+                        </li>
 
-                <li>
-                    <a href="pages/sockets.html">Sockets</a>
-                </li>
+                        <li>
+                            <a href="pages/removers.html">Removers</a>
+                        </li>
 
-                <li>
-                    <a href="pages/removers.html">Removers</a>
-                </li>
+                        <li>
+                            <a href="pages/extensions.html">Extensions</a>
+                        </li>
 
-                <li>
-                    <a href="pages/extensions.html">Extensions</a>
-                </li>
+                        <li>
+                            <a href="pages/screwdrivers.html">Screwdrivers</a>
+                        </li>
 
-                <li>
-                    <a href="pages/screwdrivers.html">Screwdrivers</a>
-                </li>
+                        <li>
+                            <a href="pages/prybars.html">pry and pic bars</a>
+                        </li>
 
-                <li>
-                    <a href="pages/prybars.html">pry and pic bars</a>
-                </li>
+                        <li>
+                            <a href="pages/cable.html">cables</a>
+                        </li>
 
-                <li>
-                    <a href="pages/cable.html">cables</a>
-                </li>
+                        <li>
+                            <a href="pages/jacks.html">Jacks</a>
+                        </li>
 
-                <li>
-                    <a href="pages/jacks.html">Jacks</a>
-                </li>
+                        <li>
+                            <a href="pages/misc.html">Misc</a>
+                        </li>
 
-                <li>
-                    <a href="pages/misc.html">Misc</a>
-                </li>
+                        <li>
+                            <a href="pages/discs.html">cut/grind wheels</a>
+                        </li>
 
-                <li>
-                    <a href="pages/discs.html">cut/grind wheels</a>
-                </li>
+                        <li>
+                            <a href="pages/clamps.html">Clamps</a>
+                        </li>
 
-                <li>
-                    <a href="pages/clamps.html">Clamps</a>
-                </li>
+                        <li>
+                            <a href="pages/chisels.html">chisels</a>
+                        </li>
 
-                <li>
-                    <a href="pages/chisels.html">chisels</a>
-                </li>
+                        <li>
+                            <a href="pages/hammers">Hammers</a>
+                        </li>
 
-                <li>
-                    <a href="pages/hammers">Hammers</a>
-                </li>
+                        <li>
+                            <a href="pages/spoons.html">spoons</a>
+                        </li>
 
-                <li>
-                    <a href="pages/spoons.html">spoons</a>
-                </li>
+                        <li>
+                            <a href="pages/visegrips.html">Vise Grips</a>
+                        </li>
 
-                <li>
-                    <a href="pages/visegrips.html">Vise Grips</a>
-                </li>
-
-                <li>
-                    <a href="pages/blades.html">Blades</a>
-                </li>
-            </ul>
+                        <li>
+                            <a href="pages/blades.html">Blades</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
         </section>
