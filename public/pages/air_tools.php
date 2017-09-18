eca70f81fae22bdfdcb35e4720fa99f768af3466
Michael Williaks <manicdesigner@gmail.com>
2017-09-18 00:06:30 -0400

    update sql statement and actions reguarding sql statement.

--- public/pages/air_tools.php
+++ public/pages/air_tools.php
@@ -6,14 +6,35 @@
  * Date: 7/13/17
  * Time: 5:47 AM
  */
-// dummy data testing !! take out for database later
-require(PRIVATE_PATH . '/dum_data.php');
-// !! future removal
+
+    require_once '../../private/connection.php';
+    if ($mysqli->connect_error) {
+        die("$mysqli->connect_errno: $mysqli->connect_error");
+    }
+    $query = 'SELECT t.item, t.price, t.sold, i.image, b.brand FROM Tools AS t JOIN Images AS i ON t.t_id = i.t_id JOIN Brands AS b ON t.b_id = b.b_id';
+    $stmt = $mysqli->query($query)
+/* prepare statement */
+
+
+    /* Bind variable for placeholder */
+
+
+
+    /* execute statement */
+  ;
+
+    printf("rows inserted: %d\n", $stmt = $mysqli->affected_rows);
+
+    /* close statement */
+    $mysqli->close();
+
+
 
 
 require(SHARED_PATH . '/header.php');
 require(SHARED_PATH . '/nav.php');
 ?>
+
 
 <section>
     <div class="row">
@@ -38,16 +59,19 @@ require(SHARED_PATH . '/nav.php');
         <section class="container-fluid">
 
             <div class="row">
-                <?php foreach ($tools as $tool) : { ?>
+
+                <?php
+                if (isset($stmt)) {
+                foreach ($stmt as $tool) : { ?>
                 <div class="col-xs-12 col-sm-6 col-md-3">
                     <article class="card">
-                        <img src="<?php echo url_for(IMAGES . '/images/med_img/air/b48_air.jpg'); ?>" alt="air tool" class="half img-thumbnail img-responsive center-block">
+                        <img src="<?php echo url_for($tool['image']); ?>" alt="air tool" class="half img-thumbnail img-responsive center-block">
                         <p class="text-center">
-                            Item: <?php echo $tool['item']; ?><br>
-                            Brand: <?php echo $tool['brand']; ?><br>
-                            Price: <?php echo $tool['price']; ?><br>
+                            Item: <?php echo $tool['i.item']; ?><br>
+                            Brand: <?php echo $tool['b.brand']; ?><br>
+                            Price: <?php echo $tool['t.price']; ?><br>
                             <button class="btn btn-default btn-lg">
-                                <a href="#"><?php echo $tool['item']; ?></a>
+                                <a href="#"><?php echo $tool['t.item']; ?></a>
                             </button>
                         </p>
 
@@ -55,20 +79,11 @@ require(SHARED_PATH . '/nav.php');
 
                 </div>
                 <?php } endforeach; ?>
+                <?php } ?>
+
             </div>
 
         </section>
     </main>
 
-
-
-
-
-
-
-
-
-
 <?php require(SHARED_PATH . '/footer.php'); ?>
-
-
