26228019da24d652d8059961fc436f5ef36eea80
Michael Williaks <manicdesigner@gmail.com>
2017-10-10 17:54:57 -0400

    reduced size of image in thumbnail with a simple styling. styled them in the sass file
    $mini-image-size-width;
    $mini-image-size-height;
    
    to negotiate height and size.

--- public/pages/air_tools.php
+++ public/pages/air_tools.php
@@ -53,7 +53,7 @@ require(SHARED_PATH . '/nav.php');
                     while ($obj = $result->fetch_object()) { ?>
                 <div class="col-xs-12 col-sm-6 col-md-3">
                     <article class="card">
-                        <img src="<?php echo url_for(IMAGES . $obj->image); ?>" alt="air tool" class="half img-thumbnail img-responsive center-block">
+                        <img src="<?php echo url_for(IMAGES . $obj->image); ?>" alt="air tool" class="box-size-images img-thumbnail img-responsive center-block">
                         <p class="text-center">
                             Item: <?php echo $obj->item; ?><br>
                             Brand: <br>
