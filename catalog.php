55df1cf01ed76a7130a719342f78193c76337b49
michael Williams <manicdesigner@gmail.com>
2017-12-21 17:36:50 -0500

    add the foreach loop and sections for holding the pieces that are going to be retrieved by the $tool and $obj variables.

--- catalog.php
+++ catalog.php
@@ -8,34 +8,10 @@
  * Time: 7:52 PM
  */
 
-$cats_table = [
-    'wrenches' => 'Wrenches',
-    'files' => 'Files',
-    'bits' => 'Bits',
-    'air_tools' => 'Air_tools',
-    'ratchets' => 'Ratchets',
-    'crimps' => 'Crimps',
-    'drills' => 'Drills',
-    'sockets' => 'Sockets',
-    'removers' => 'Removers',
-    'extensions' => 'Extensions',
-    'screwdrivers' => 'Screwdrivers',
-    'bars' => 'Bars',
-    'cables' => 'Cables',
-    'jacks' => 'Jacks',
-    'misc' => 'Misc',
-    'discs' => 'Wheels',
-    'cch' => 'Hcc',
-    'chisels' => 'Chisels',
-    'hammers' => 'Hammers',
-    'spoons' => 'Spoons',
-    'vise_grips' => 'Visegrips',
-    'blades' => 'Blades'
-];
 
 
-$query = "SELECT t.item as item, t.price as price, t.sold as sold, i.image as image, b.brand as brand, c.category as category FROM Tools AS t JOIN Images AS i ON t.t_id = i.t_id JOIN Brands AS b ON t.b_id = b.b_id JOIN Categories AS c ON t.c_id = c.c_id WHERE category like 'pry%'";
 ?>
+
 <?php
   if ( isset($_GET['cat']) ) {
       if ( $_GET['cat'] == 'wrenches') {
@@ -113,25 +89,26 @@ require(SHARED_PATH . '/nav.php');
 
         <div class="row">
 
-            <?php
-            if ($result = $mysqli->query($query)) {
-                while ($obj = $result->fetch_object()) { ?>
+<!--            --><?php
+                    foreach($tool as $obj)  {
+                    ?>
                     <div class="col-xs-12 col-sm-6 col-md-3">
                         <article class="card">
-                            <img src="<?php echo url_for($obj->image); ?>" alt="air tool" class="box-image-width box-image-height img-thumbnail img-responsive center-block">
+<!--                            <img src="--><?php //echo url_for($obj->image); ?><!--" alt="air tool" class="box-image-width box-image-height img-thumbnail img-responsive center-block">-->
                             <p class="text-center">
-                                Item: <?php echo $obj->item; ?><br>
+                                Item: <?php echo $obj->name; ?><br>
                                 Brand: <br>
                                 Price: <?php echo $obj->price; ?><br>
-                                <button class="btn btn-default btn-lg" value="<?php echo $obj->item; ?>">
-                                    <a href="#"><?php echo $obj->item; ?></a>
+                                <button class="btn btn-default btn-lg" value="<?php echo $obj->name; ?>">
+                                    <a href="#"><?php echo $obj->code; ?></a>
                                 </button>
                             </p>
                         </article>
                     </div>
-                <?php } // end inner while block?>
-                <?php  mysqli_free_result($result); ?>
-            <?php   } ?>
+                    <?php }  ?>
+
+
+
 
 
 
