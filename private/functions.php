2fc7ce5b1c990b86882b21d96e63ec9e4c2547dd
michael Williams <manicdesigner@gmail.com>
2017-12-22 11:32:37 -0500

    added function for array_category sorting items.

--- private/functions.php
+++ private/functions.php
@@ -26,7 +26,7 @@ function single_item_query($id) {
         if(isset($db))
         $tools = $db->prepare("SELECT t.item_code as code, t.item_name as name, t.retail_price as retail,
                                 t.sale_price as price, t.item_pieces as pieces, t.qty as quantity,
-                                 t.sold as sold, b.brand as brand, c.category as category 
+                                 t.sold as sold, b.brand as brand, c.category as category, tt.tool_type as tool_type 
                                  FROM Tools AS t 
                                  JOIN Brands AS b ON t.b_id = b.b_id 
                                  JOIN Categories AS c ON t.c_id = c.c_id 
@@ -59,4 +59,21 @@ function select_query () {
         exit();
     }
     return $tool;
+}
+
+function array_category($catalog, $category) {
+    $output = array();
+
+    foreach($catalog as $id => $item) {
+        if ( $category == null || strtolower($category) == strtolower($tool['tool_type'])) {
+            $sort = $item['name'];
+            $sort = ltrim($sort, 'The ');
+            $sort = ltrim($sort, 'A ');
+            $sort = ltrim($sort, 'An ');
+            $tool[$id] = $sort;
+        }
+    }
+    asort($output);
+    return array_keys($output);
+
 }
