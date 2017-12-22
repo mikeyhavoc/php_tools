18e0559d571e6b36726ea96a1c4a200000f35572
michael Williams <manicdesigner@gmail.com>
2017-12-21 23:14:25 -0500

    add sections for sql statements.

--- catalog.php
+++ catalog.php
@@ -10,58 +10,83 @@
 
 
 
+
 ?>
 
 <?php
   if ( isset($_GET['cat']) ) {
       if ( $_GET['cat'] == 'wrenches') {
           $page_name = 'Wrenches';
+          $section = 'wrenches';
       } elseif ( $_GET['cat'] == 'files') {
           $page_name = 'Files';
+          $section = 'files';
       } elseif ( $_GET['cat'] == 'pliers') {
           $page_name = 'Pliers';
+          $section = 'pliers';
       } elseif ( $_GET['cat'] == 'bits') {
           $page_name = 'Bits';
+          $section = 'bits';
       } elseif ( $_GET['cat'] == 'air_tools') {
           $page_name = 'Air Tools';
+          $section = 'air-tools';
       } elseif ( $_GET['cat'] == 'ratchets') {
           $page_name = 'Ratchets';
+          $section = 'ratchets';
       } elseif ( $_GET['cat'] == 'crimps') {
           $page_name = 'Crimps';
+          $section = 'crimps';
       } elseif ( $_GET['cat'] == 'drills') {
           $page_name = 'Drills';
+          $section = 'drills';
       } elseif ( $_GET['cat'] == 'sockets') {
           $page_name = 'Sockets';
+          $section = 'sockets';
       } elseif ( $_GET['cat'] == 'removers') {
           $page_name = 'Removers';
+          $section = 'removers';
       } elseif ( $_GET['cat'] == 'extensions') {
           $page_name = 'Extensions';
+          $section = 'extensions';
       } elseif ( $_GET['cat'] == 'screwdrivers') {
           $page_name = 'Screwdrivers';
+          $section = 'screwdrivers';
       } elseif ( $_GET['cat'] == 'bars') {
           $page_name = 'Pry and Pic Bars';
+          $section = 'bars';
       } elseif ( $_GET['cat'] == 'cables') {
           $page_name = 'Cables';
+          $section = 'cables';
       } elseif ( $_GET['cat'] == 'jacks') {
           $page_name = 'Jacks';
+          $section = 'jacks';
       } elseif ( $_GET['cat'] == 'misc') {
           $page_name = 'Misc';
+          $section = 'misc';
       } elseif ( $_GET['cat'] == 'discs') {
           $page_name = 'Discs';
+          $section = 'wheels';
       } elseif ( $_GET['cat'] == 'cch') {
           $page_name = 'Clamps/Chains/Hooks';
+          $section = 'hcc';
       } elseif ( $_GET['cat'] == 'chisels') {
           $page_name = 'Chisels';
+          $section = 'chisels';
       } elseif ( $_GET['cat'] == 'hammers') {
           $page_name = 'Hammers';
+          $section = 'hammers';
       } elseif ( $_GET['cat'] == 'spoons') {
           $page_name = 'Spoons';
+          $section = 'spoons';
       } elseif ( $_GET['cat'] == 'vise_grips') {
           $page_name = 'Vise Grips';
+          $section = 'vise_grips';
       } elseif ( $_GET['cat'] == 'blades') {
           $page_name = 'Blades';
+          $section = 'blades';
       } else {
           $page_name = 'Full Catalog';
+          $section = null;
       }
 
   }
@@ -90,7 +115,8 @@ require(SHARED_PATH . '/nav.php');
         <div class="row">
 
 <!--            --><?php
-                    foreach($tool as $obj)  {
+                    $tools = select_query();
+                    foreach($tools as $obj)  {
                     ?>
                     <div class="col-xs-12 col-sm-6 col-md-3">
                         <article class="card">
