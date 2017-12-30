f48cc2891e70c773bc0c577b3ecda7b3caa3bd01
michael Williams <manicdesigner@gmail.com>
2017-12-29 19:00:23 -0500

    added a section and a param for each placement in the categories.

--- catalog.php
+++ catalog.php
@@ -11,58 +11,103 @@
 
 
 
-$query = "SELECT t.item as item, t.price as price, t.sold as sold, i.image as image, b.brand as brand, c.category as category FROM Tools AS t JOIN Images AS i ON t.t_id = i.t_id JOIN Brands AS b ON t.b_id = b.b_id JOIN Categories AS c ON t.c_id = c.c_id WHERE category like 'pry%'";
+
 ?>
 <?php
   if ( isset($_GET['cat']) ) {
       if ( $_GET['cat'] == 'wrenches') {
           $page_name = 'Wrenches';
+          $section = 'wrenches';
+          $param = 'wrenches';
       } elseif ( $_GET['cat'] == 'files') {
           $page_name = 'Files';
+          $section = 'files';
+          $param = 'files';
       } elseif ( $_GET['cat'] == 'pliers') {
           $page_name = 'Pliers';
+          $section = 'pliers';
+          $param = 'pliers';
       } elseif ( $_GET['cat'] == 'bits') {
           $page_name = 'Bits';
+          $section = 'bits';
+          $param = 'bits';
       } elseif ( $_GET['cat'] == 'air_tools') {
           $page_name = 'Air Tools';
+          $section = 'air-tools';
+          $param = 'air-tools';
       } elseif ( $_GET['cat'] == 'ratchets') {
           $page_name = 'Ratchets';
+          $section = 'ratchets';
+          $param = 'ratchets';
       } elseif ( $_GET['cat'] == 'crimps') {
           $page_name = 'Crimps';
-      } elseif ( $_GET['cat'] == 'drills') {
-          $page_name = 'Drills';
+          $section = 'crimps';
+          $param = 'crimps';
+
       } elseif ( $_GET['cat'] == 'sockets') {
           $page_name = 'Sockets';
+          $section = 'sockets';
+          $param = 'sockets';
       } elseif ( $_GET['cat'] == 'removers') {
           $page_name = 'Removers';
+          $section = 'removers';
+          $param = 'removers';
       } elseif ( $_GET['cat'] == 'extensions') {
           $page_name = 'Extensions';
+          $section = 'extensions';
+          $param = 'extensions';
       } elseif ( $_GET['cat'] == 'screwdrivers') {
           $page_name = 'Screwdrivers';
+          $section = 'screwdrivers';
+          $param = 'screwdrivers';
       } elseif ( $_GET['cat'] == 'bars') {
           $page_name = 'Pry and Pic Bars';
+          $section = 'bars';
+          $param = 'bars';
       } elseif ( $_GET['cat'] == 'cables') {
           $page_name = 'Cables';
+          $section = 'cables';
+          $param = 'cables';
       } elseif ( $_GET['cat'] == 'jacks') {
           $page_name = 'Jacks';
+          $section = 'jacks';
+          $param = 'jacks';
       } elseif ( $_GET['cat'] == 'misc') {
           $page_name = 'Misc';
+          $section = 'misc';
+          $param = 'misc';
       } elseif ( $_GET['cat'] == 'discs') {
           $page_name = 'Discs';
+          $section = 'wheels';
+          $param = 'wheels';
       } elseif ( $_GET['cat'] == 'cch') {
           $page_name = 'Clamps/Chains/Hooks';
+          $section = 'hcc';
+          $param = 'hcc';
       } elseif ( $_GET['cat'] == 'chisels') {
           $page_name = 'Chisels';
+          $section = 'chisels';
+          $param = 'chisels';
       } elseif ( $_GET['cat'] == 'hammers') {
           $page_name = 'Hammers';
+          $section = 'hammers';
+          $param = 'hammers';
       } elseif ( $_GET['cat'] == 'spoons') {
           $page_name = 'Spoons';
+          $section = 'spoons';
+          $param = 'spoons';
       } elseif ( $_GET['cat'] == 'vise_grips') {
           $page_name = 'Vise Grips';
+          $section = 'vise_grips';
+          $param = 'vise_grips';
       } elseif ( $_GET['cat'] == 'blades') {
           $page_name = 'Blades';
+          $section = 'blades';
+          $param = 'blades';
       } else {
           $page_name = 'Full Catalog';
+          $section = null;
+
       }
 
   }
@@ -90,13 +135,21 @@ require(SHARED_PATH . '/nav.php');
 
         <div class="row">
 
-            <?php  $categories = array_category($tools, $section);
-                    $item = get_item_html($id, $categories[$id]);  ?>
+            <?php  $item = multi_item_query();
 
+
+
+            ?>
+                    <?php foreach ( $item as $tools) {
+                         ?>
                     <div class="col-xs-12 col-sm-6 col-md-3">
                         <article class="card">
-                            <?php print_r($item); ?>
+                           <?php $tool = test_data($tools);
+                           echo $tool;
+                           ?>
                         </article>
+                    </div>
+                        <?php } ?>
 
 
 
