7cc0ef072a95d8e08e8169c88d358466ecddbdc1
michael Williams <manicdesigner@gmail.com>
2017-12-30 10:34:02 -0500

    working on sanitizing data for input and output.

--- details.php
+++ details.php
@@ -8,9 +8,11 @@
 
 
 if (isset($_get['t_id'])) {
-    $id = filter_input(INPUT_GET, $test, FILTER_SANITIZE_NUMBER_INT);
-    $tool = single_item_query($id);
-    var_dump($tool);
+    $id_num = $_GET['t_id'];
+    $id = filter_input(INPUT_GET, $id_num, FILTER_SANITIZE_NUMBER_INT);
+
+
+
 }
 
 //if (!isset($item)) {
@@ -18,18 +20,25 @@ if (isset($_get['t_id'])) {
 //    exit;
 //}
 
-$pageTitle = $item->name;
-$section = null;
+$pageTitle = 'Details';
 
 include(SHARED_PATH . '/header.php');
 include (SHARED_PATH . '/nav.php');
 
 ?>
 
-<?php
+<article>
+    <div class="container">
+        <div class="row">
+         <div class="col-xs-12 col-sm-6 col-md4">
+             <?php
 
-    ?>
+             ?>
 
+         </div>
+        </div>
+    </div>
+</article>
 
 
 
