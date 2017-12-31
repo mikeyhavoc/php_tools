270a88e5cc499e43bea24e1fbe6ba7539833f106
michael Williams <manicdesigner@gmail.com>
2017-12-30 22:34:30 -0500

    altering to work on getting images to show up.

--- details.php
+++ details.php
@@ -12,7 +12,11 @@ if (isset($_GET['id'])) {
     $id_num = $_GET['id'];
     $id = filter_var($id_num, FILTER_SANITIZE_NUMBER_INT);
     $item_result = single_item_query($id);
+    $image_results = single_item_images_query($item_result);
 
+
+
+    print_r($item_result);
 
 }
 
@@ -32,13 +36,19 @@ include (SHARED_PATH . '/nav.php');
 <article>
     <div class="container">
         <div class="row">
-         <div class="col-xs-12 col-sm-6 col-md4">
+            <div class="col-xs-12 col-sm-6">
              <?php
-                $item = detail_item($item_result);
+                $item = detail_single_item($item_result);
                 echo $item;
              ?>
-
-         </div>
+            </div>
+            <?php  $pics = detail_images($item_result);
+                 foreach($image_results as $pic) { ?>
+            <div class="col-xs-12 col-sm-6">
+                <?php $image = detail_images($pics);
+                      echo $image; ?>
+            </div>
+                     <?php } ?>
         </div>
     </div>
 </article>
