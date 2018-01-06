86d7f5ec77e8659cb9a1fffe1de886224d5099b0
michael Williams <manicdesigner@gmail.com>
2018-01-06 07:42:13 -0500

    fixing double redundancy.

--- private/shared/nav.php
+++ private/shared/nav.php
@@ -1,49 +1,49 @@
 
         <nav class=" hidden-sm hidden-xs col-xs-12 col-sm-10">
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=wrenches'); ?>">Wrenches</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=wrenches'); ?>">Wrenches</a>
 
-            <a  class="btn btn-lg btn-default href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=files'); ?>">Files</a>
+            <a  class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=files'); ?>">Files</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=pliers'); ?>">Pliers</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=pliers'); ?>">Pliers</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=bits'); ?>">Bits</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=bits'); ?>">Bits</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=air_tools'); ?>">Air Tools</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=air_tools'); ?>">Air Tools</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=ratchets'); ?>">Ratchets</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=ratchets'); ?>">Ratchets</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=crimps'); ?>">Crimps/Cutters</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=crimps'); ?>">Crimps/Cutters</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=sockets'); ?>">Sockets</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=sockets'); ?>">Sockets</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=removers'); ?>">Removers</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=removers'); ?>">Removers</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=extensions'); ?>">Extensions</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=extensions'); ?>">Extensions</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=screwdrivers'); ?>">Screwdrivers</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=screwdrivers'); ?>">Screwdrivers</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=bars'); ?>">pry and pic bars</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=bars'); ?>">pry and pic bars</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=cables'); ?>">cables</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=cables'); ?>">cables</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=jacks'); ?>">Jacks</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=jacks'); ?>">Jacks</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=misc'); ?>">Misc Tools</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=misc'); ?>">Misc Tools</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=discs'); ?>">Discs</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=discs'); ?>">Discs</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=cch'); ?>">Clamps/Chains/Hooks</a></button>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=cch'); ?>">Clamps/Chains/Hooks</a></button>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=chisels'); ?>">chisels</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=chisels'); ?>">chisels</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=hammers'); ?>">Hammers</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=hammers'); ?>">Hammers</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=spoons'); ?>">spoons</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=spoons'); ?>">spoons</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=vise_grips'); ?>">Vise Grips</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=vise_grips'); ?>">Vise Grips</a>
 
-            <a class="btn btn-lg btn-default" href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=blades'); ?>">Blades</a>
+            <a class="btn btn-lg btn-default" href="<?php echo url_for( 'catalog.php?cat=blades'); ?>">Blades</a>
 
         </nav>
 
@@ -54,93 +54,93 @@
                     <button class="btn btn-default dropdown-toggle type=button" id="minimenu" data-toggle="dropdown"><span class="caret"></span></button>
                     <ul class="dropdown-menu" role="menu" aria-labeledby="minimenu">
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=wrenches'); ?>">Wrenches</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=wrenches'); ?>">Wrenches</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=files'); ?>">Files</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=files'); ?>">Files</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=pliers'); ?>">Pliers</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=pliers'); ?>">Pliers</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=bits'); ?>">Bits</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=bits'); ?>">Bits</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=air_tools'); ?>">Air Tools</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=air_tools'); ?>">Air Tools</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=ratchets'); ?>">Ratchets</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=ratchets'); ?>">Ratchets</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=crimps'); ?>">Crimps/Cutters</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=crimps'); ?>">Crimps/Cutters</a>
                         </li>
 
 
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=sockets'); ?>">Sockets</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=sockets'); ?>">Sockets</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=removers'); ?>">Removers</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=removers'); ?>">Removers</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=extensions'); ?>">Extensions</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=extensions'); ?>">Extensions</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=screwdrivers'); ?>">Screwdrivers</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=screwdrivers'); ?>">Screwdrivers</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=bars'); ?>">pry and pic bars</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=bars'); ?>">pry and pic bars</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=cables'); ?>">cables</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=cables'); ?>">cables</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=jacks'); ?>">Jacks</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=jacks'); ?>">Jacks</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=misc'); ?>">Misc Tools</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=misc'); ?>">Misc Tools</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=discs'); ?>">Discs</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=discs'); ?>">Discs</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=cch'); ?>">Clamps/hooks/chains</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=cch'); ?>">Clamps/hooks/chains</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=chisels'); ?>">chisels</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=chisels'); ?>">chisels</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=hammers'); ?>">Hammers</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=hammers'); ?>">Hammers</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=spoons'); ?>">spoons</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=spoons'); ?>">spoons</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=vise_grips'); ?>">Vise Grips</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=vise_grips'); ?>">Vise Grips</a>
                         </li>
 
                         <li>
-                            <a href="<?php echo url_for(WWW_ROOT . 'catalog.php?cat=blades'); ?>">Blades</a>
+                            <a href="<?php echo url_for( 'catalog.php?cat=blades'); ?>">Blades</a>
                         </li>
                     </ul>
                 </div>
