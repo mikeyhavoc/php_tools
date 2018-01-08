d4c8a952d3f10a4228bfe35b287d7534c90bcf5f
michael Williams <manicdesigner@gmail.com>
2018-01-08 15:19:19 -0500

    updated the gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-'use  strict';
+'use  strict'
 
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+let path = require('path');
+const sass = require('gulp-ruby-sass');
 
-gulp.task('concatScripts', () =>
-   gulp.src([
-     'js/vendor/modernizr-2.8.9-respond-1.4.2.min.js',
-     'js/main.js'
-   ])
-);
+
+gulp.task('sass', () =>
+    sass('sass/main.scss')
+        .on('error', sass.logError)
+        .pipe(gulp.dest('public/css/')));
