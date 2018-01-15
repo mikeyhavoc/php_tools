8798661c1bda4ed657702b7cef2950e86d563202
Michael Williams <manicdesigner@gmail.com>
2018-01-15 10:41:30 -0500

    getting rid of all excess spaces.
    Signed-off-by: Michael Williams <manicdesigner@gmail.com>

--- details.php
+++ details.php
@@ -8,14 +8,9 @@
  * Date: 12/21/17
  * Time: 11:52 PM
  */
-
-
-
 $id_num = $_GET['id']; // grabbing Global Variable GET id
 $id = filter_var($id_num, FILTER_SANITIZE_NUMBER_INT); // filtering and sanitizing number going into array for value.
 $con = $db; // the connection to the db.
-
-
 // single item SQL Query.
 try {
     $single_item_full_query = "SELECT t.item_code AS code, t.item_name AS name, 
@@ -62,23 +57,9 @@ try {
 }catch(PDOException $e) {
     echo $e->getMessage();
 }
-
-
-
-
-
-
-
-
-
-
-
-
 $pageTitle = 'Details';
-
 include(SHARED_PATH . '/header.php');
 include (SHARED_PATH . '/nav.php');
-
 ?>
 <div class="row">
     <div class="col-12">
