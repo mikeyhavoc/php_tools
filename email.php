40050f1fad4ae9a7ef202626ac5972d7ccc9f28f
michael Williams <manicdesigner@gmail.com>
2018-01-19 12:41:54 -0500

    setting up email php file.

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright (c) 2018. Michael Williams Manic Designer Developments.
+ */
+require_once ('private/initialize.php');
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 1/19/18
+ * Time: 12:33 PM
+ */
+
+require(SHARED_PATH . '/header.php');
+require(SHARED_PATH . '/nav.php');
+
+?>
+<div class="container-fluid">
