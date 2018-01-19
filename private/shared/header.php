634242558c71eb01705bb5966c94dd3325a39b42
michael Williams <manicdesigner@gmail.com>
2018-01-19 07:32:38 -0500

    changed goole font.

--- private/shared/header.php
+++ private/shared/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Sirin+Stencil" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Supermercado+One" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo url_for('public/css/bootstrap-full.css'); ?>">
    <!--<link rel="stylesheet" href="css/layout.css">
     <link rel="stylesheet" href="css/helpers.css">-->
@@ -17,8 +17,9 @@
 <body>
     <header role="banner" class='container-fluid'>
       <div class='row'>
-        <h1 class="col-xs-12 col-md-6 tools">
-            <a href="<?php echo url_for( 'index.php'); ?>">Garys Tool List</a>
+        <h1 class="col-12 col-md-6 tools">
+            <a href="<?php echo url_for( 'index.php'); ?>">Garys Tools</a>
         </h1>
+      </div>
      </header>
 
