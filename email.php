31461b4ce5a7fdfafa9a3747457e27d8ba526bc1
michael Williams <manicdesigner@gmail.com>
2018-01-22 17:03:51 -0500

    rm php mailer for now. added back the reuest method if == post ready to send mail now. and responds with a thank you for sending email. in location:email.php?status=thanks.php added page_title = tool inquiry. reworked the layout for a better responsive design layout.

--- email.php
+++ email.php
@@ -1,8 +1,37 @@
-<?php use PHPMailer\PHPMailer\PHPMailer;
-      //use PHPMailer\PHPMailer\Exception;
-require_once ('private/initialize.php');
-require './vendor/autoload.php';
+<?php require_once 'private/initialize.php';
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\Exception;
+//require_once ('private/initialize.php');
+//require './vendor/autoload.php';
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $first_name = trim(filter_input(INPUT_POST, 'first-name', FILTER_SANITIZE_STRING));
+    $last_name = trim(filter_input(INPUT_POST, 'last-name', FILTER_SANITIZE_STRING));
+    $email = trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
+    $phone_number = trim(filter_input(INPUT_POST, 'contact-number', FILTER_SANITIZE_STRING));
+    $subject = trim(filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING));
+    $inquiry_questions = trim(filter_input(INPUT_POST, 'inquiry-questions', FILTER_SANITIZE_SPECIAL_CHARS));
+    $full_name = $first_name . ' ' . $last_name;
 
+    $email_body = "<pre><br>";
+    $email_body .= 'Full Name:' . $full_name . "<br>";
+    $email_body .= 'Email: ' . $email . "<br>";
+    $email_body .= 'Phone number: ' . $phone_number . "<br>";
+    $email_body .= 'Subject: ' . $subject . "<br>";
+    $email_body .= 'Inquiry: ' . $inquiry_questions . "<br>";
+    $email_body .= "<pre>";
+
+
+
+    //TODO. send email.
+    header("location:email.php?status=thanks");
+}
+
+
+
+
+
+$page_title = 'Tool Inquiry';
+$section = null;
 /**
  * Copyright (c) 2018. Michael Williams Manic Designer Developments.
  */
@@ -12,105 +41,38 @@ require './vendor/autoload.php';
  * Date: 1/19/18
  * Time: 2:26 PM
  */
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $first_name = trim(filter_input(INPUT_POST, 'first-name', FILTER_SANITIZE_STRING));
-    $last_name = trim(filter_input(INPUT_POST, 'last-name', FILTER_SANITIZE_STRING));
-    $email = trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
-    $phone_number = trim(filter_input(INPUT_POST, 'contact-number', FILTER_SANITIZE_STRING));
-    $subject = trim(filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING));
-    $inquiry_questions = trim(filter_input(INPUT_POST, 'inquiry-questions', FILTER_SANITIZE_SPECIAL_CHARS));
-    $full_name = $first_name . ' ' . $last_name;
-//    if ($first_name == '' || $last_name == '' || $email == '' || $phone_number == '' || $inquiry_questions == '') { echo 'Please enter first name';
-//        exit;
-//    }
-    if ($_POST['address'] != '') {
-        echo 'Bad form input';
-        exit;
-    }
-//    if (!PHPMailer::validateAddress($email)) {
-//        echo "Invalid Email Address";
-//        exit;
-//    }
-
-//    if ($last_name == '') { echo "Please enter Last Name"; }
-
-//    $email_body = "";
-//
-//    $email_body .= 'First Name ' . $first_name . "<br>";
-//    $email_body .= 'last name ' . $last_name . "<br>";
-//    $email_body .= 'email ' . $email . "<br>";
-//    $email_body .= 'phone number ' . $phone_number . "<br>";
-//    $email_body .= 'questions ' . $inquiry_questions . "<br>";
-//
-//    echo $email_body;
-    $to = 'garyws.items@gmail.com';
-
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->SMTPDebug = 2;
-
-    $mail->Port = 587;
-    $mail->SMTPSecure = 'tls';
-    $mail->SMTPAuth = 'true';
-
-    $mail->Password = $pass;
-    $mail->setFom('from@example.com', 'test person');
-    $mail->addReplyTo($email, $full_name);
-
-    // test subject change in future if works.
-    $mail->Subject = $subject;
-    $mail->Body = $email_body;
-    if (!$mail->send()) {
-        $msg .= 'Mailer error ' . $mail->ErrorInfo();
-    }
-    header('location:email.php?status=thanks');
-}
-
-
-
-
-
-/**
- * Copyright (c) 2018. Michael Williams Manic Designer Developments.
- */
-
-/**
- * Created by PhpStorm.
- * User: mike
- * Date: 1/19/18
- * Time: 12:33 PM
- */
-
-require(SHARED_PATH . '/header.php');
-require(SHARED_PATH . '/nav.php');
 
 ?>
-<div class="container-fluid">
-    <?php if (isset($_POST['status']) && $_POST['status'] == 'thanks') { ?>
-         <div class="container-fluid">
-        <div class="col-12 text-center">
-            <h1>Thank you for inquiry or question.</h1>
-            <p>We will try to reach you as soon as possible to complete the deal.</p>
+<?php   require (SHARED_PATH . '/header.php');
+            require (SHARED_PATH . '/nav.php');
+    ?>
+   <?php if (isset($_GET['status']) && $_GET['status'] == 'thanks') { ?>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12">
+            <h2>Thank you for your interest in my tools!</h2>
+                <p>I will try to be as prompt as possible in responding back to your questions or inquiry in purchase.</p>
+            </div>
         </div>
     </div>
-   <?php } else { ?>
-
+    <?php } else { ?>
+    <div>
     <form action="email.php" method="POST">
-        <div class="container">
+        <div class="container-fluid text-center">
             <div class="row">
-                <div class="col-12 text-center m-3">
+                <div class="col-12 my-2  my-sm-3">
                     <label for="first-name">First Name</label>
                     <input type="text" id="first-name" name="first-name" placeholder="First name" required>
                 </div>
-                <div class="col-12 text-center m-3">
+                <div class="col-12 my-2 my-sm-3">
                     <label for="last-name">Last Name</label>
                     <input type="text" id="last-name" name="last-name" placeholder="Last Name">
                 </div>
-                <div class="col-12 text-center m-3">
+                <div class="col-12 my-2 my-sm-3">
                     <label for="email">Email</label>
                     <input type="email" id="email" name="email" placeholder="Email">
                </div>
-                <div class="col-12 text-center m-3">
+                <div class="col-12 my-2 my-sm-3">
                     <label for="area-near">Area Near or Located</label>
                     <select name="area-near" id="area-near" required>
                         <option value="">select</option>
@@ -120,33 +82,33 @@ require(SHARED_PATH . '/nav.php');
                         <option value="palmetto">Palmetto</option>
                     </select>
                 </div>
-                <div class="col-12 text-center m-3">
+                <div class="col-12 my-2 my-sm-3">
                     <label for="contact-number">Contact Number</label>
-                    <input type="tel" id="contact-number" name="contact-number" placeholder="941-000-0000">
+                    <input type="tel" id="phone-number" name="contact-number" placeholder="941-000-0000">
                     <div style="display:none;">
                         <label for="address">address</label>
                         <input type="text" id="address" name="address">
                         <p>please leave this field blank</p>
                     </div>
                 </div>
-                <div class="col-12 text-center m3">
+                <div class="col-12 my-2 my-sm-3">
                     <label for="subject">Subject:</label>
                     <input type="text" id="subject" name="subject">
                 </div>
-                <div class="col-12 text-center m-3">
+                <div class="col-12 my-2 my-sm-3">
                     <label for="inquiry-questions">Questions Inquiries</label><br>
                     <textarea name="inquiry-questions" id="inquiry-questions" cols="30" rows="10" placeholder="Questions about an item or an offer on an item.."></textarea>
                 </div>
-                    <div class="col-12 text-center m-3 ">
+                    <div class="col-12 my-2 my-sm-3 ">
                         <input class="btn btn-lg btn-outline-success submit-width"  type="submit" value="submit">
                     </div>
-                    <div class="col-12 text-center m-3 ">
+                    <div class="col-12 my-2  my-sm-3 ">
                         <input class="btn btn-lg btn-outline-danger submit-width" type="reset" value="reset">
                     </div>
 
             </div>
         </div>
     </form>
-    <?php } ?>
+   <?php } ?>
 </div>
 <?php include (SHARED_PATH . '/footer.php'); ?>
