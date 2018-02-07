248e7c2c81958aa071ca1ebc70842e0105dff093
michael williams <manicdesigner@gmail.com>
2018-02-06 20:57:50 -0500

    complete revamp for src folder and dist folder and using a watch gulp item.

--- gulpfile.js
+++ gulpfile.js
@@ -6,54 +6,73 @@ const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
 const imagemin = require('gulp-imagemin');
-const {phpMinify} = require('@cedx/gulp-php-minify');
+const phpMinify = require('@cedx/gulp-php-minify');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const pump = require('pump');
+const gutil = require('gutil');
 
-gulp.task('sass', () =>
-    sass('sass/main.scss')
+gulp.task('sass', function () {
+    sass('./src/public/sass/**/*.scss')
         .on('error', sass.logError)
-        .pipe(gulp.dest('sass/')));
+        .pipe(gulp.dest('./dist/public/sass/'))
+});
 
-gulp.task('style', function () {
-    return gulp.src('sass/**/*.css')
+
+gulp.task('build-css', function () {
+    return gulp.src('./src/public/sass/**/*.css')
         .pipe(sourcemaps.init())
         .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest('public/css/'));
+        .pipe(gulp.dest('./dist/public/css/'));
 
 });
 
-
+gulp.task('minify-css',() => {
+    return gulp.src('./src/public/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('./dist/public/css/maps/'))
+        .pipe(gulp.dest('./dist/public/css/'));
+});
 
 gulp.task('images', function () {
-    return gulp.src('./public/img/**/*.jpg')
+    return gulp.src('./src/public/img/**/*.jpg')
         .pipe(imagemin({
             progressive: true
         }))
-        .pipe(gulp.dest('./dist/imgs/'));
+        .pipe(gulp.dest('./dist/public/imgs/'));
 });
 
-gulp.task('minifyphp', () => gulp.src('./**/*.php', {read: false})
+function minifyJS(sourceFiles, destinationFolder, filenameRoot) {
+    return gulp.src(sourceFiles)
+        .pipe(plumber())
+    return gulp.src(sourceFiles)
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(concat(filenameRoot + '.js'))
+        .pipe(gulp.dest(destinationFolder)) // save .js
+        .pipe(uglify({ preserveComments: 'license' }))
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest(destinationFolder)) // save .min.js
+
+gulp.task('minifyphp', () => gulp.src('.src/**/*.php', {read: false})
     .pipe(phpMinify())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./dist/'))
 );
 
-gulp.task('minify-css',() => {
-    return gulp.src('./public/**/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/public/'));
-});
 
-gulp.task('compress', function (cb) {
-    pump([
-            gulp.src('./public/js/**/*.js'),
-            uglify(),
-            gulp.dest('./dist/public/')
-        ],
-        cb
-    );
+
+
+
+gulp.task('watch', function () {
+    gulp.watch('./src/public/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/public/sass/**/*.css', ['build-css']);
+    gulp.watch('./src/public/**/*.css', ['minify-css']);
+    gulp.watch('./src/public/img/**/*.jpg', ['images']);
+    gulp.watch('./src/public/js/**/*.js', ['compress']);
+    gulp.watch('.src/**/*.php', ['minifyphp']);
+
+
 });
