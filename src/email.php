4a7b27a4dd0458fdaf6cb6a9cb21342a1cb9deda
michael williams <manicdesigner@gmail.com>
2018-03-31 22:56:07 -0400

    clean up file

--- src/email.php
+++ src/email.php
@@ -1,62 +1,69 @@
 <?php require_once 'private/initialize.php';
-
-
- require '/vendor/autoload.php';
-
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+//Create a new PHPMailer instance
 $mail = new PHPMailer;
-//Tell PHPMailer to use SMTP - requires a local mail server
-//Faster and safer than using mail()
+//Tell PHPMailer to use SMTP
 $mail->isSMTP();
-$mail->Host = 'localhost';
-$mail->Port = 25;
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "*****@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = '*****';
 
 
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $first_name = trim(filter_input(INPUT_POST, 'first-name', FILTER_SANITIZE_STRING));
-        $last_name = trim(filter_input(INPUT_POST, 'last-name', FILTER_SANITIZE_STRING));
-        $email = trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
-        $phone_number = trim(filter_input(INPUT_POST, 'phone-number', FILTER_SANITIZE_STRING));
-        $subject = trim(filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING));
-        $inquiry_questions = trim(filter_input(INPUT_POST, 'inquiry-questions', FILTER_SANITIZE_SPECIAL_CHARS));
-        $full_name = $first_name . ' ' . $last_name;
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $first_name = trim(filter_input(INPUT_POST, 'first-name', FILTER_SANITIZE_STRING));
+    $last_name = trim(filter_input(INPUT_POST, 'last-name', FILTER_SANITIZE_STRING));
+    $email = trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
+    $phone_number = trim(filter_input(INPUT_POST, 'phone-number', FILTER_SANITIZE_STRING));
+    $subject = trim(filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING));
+    $inquiry_questions = trim(filter_input(INPUT_POST, 'inquiry-questions', FILTER_SANITIZE_SPECIAL_CHARS));
+    $full_name = $first_name . ' ' . $last_name;
 
-
-        $mail->setFrom('garywsitems@gmail.com', 'seller tools');
-        $mail->addAddress('garywsitems@gmail.com', 'Gary williams');
-
-        if ($mail->addReplyTo($email, $first_name)) {
-            $mail->Subject = $subject;
-            $mail->isHTML(false);
-
-            $mail->Body =<<<EOT
-           Name: {$full_name}
-           Email: {$email}
-           Phone: {$phone_number}
-           Subject: {$subject}
-           Inquiry: {$inquiry_questions}         
-EOT;
-
-        }// send message check for errors;
-        if (!$mail->send()) {
-            // reason for failing send be in $mail->ErrInfo()
-            // log in server.
-            $msg = 'Sorry, Something went wrong. Please try again later.';
-        } else  {
-            $msg = 'thank you for your interest in our tools!';
-            $msg .= 'We will try to get back to you as soon as possible!';
-            //header("location:email.php?status=thanks");
-        }
-
-        //TODO. send email.
-
-    }else {
-        $msg = 'Invalid email address, message ignored.';
-    }
+    $email_body .= 'Full Name:' . $full_name . "\n";
+    $email_body .= 'Email: ' . $email . "\n";
+    $email_subject = $subject;
+    $email_body .= 'Phone number: ' . $phone_number . "\n";
+    $email_body .= 'Subject: ' . $subject . "\n";
+    $email_body .= 'Inquiry: ' . $inquiry_questions . "\n";
 
 
 
+//Set who the message is to be sent from
+    $mail->setFrom($email, $first_name);
+//Set an alternative reply-to address
+    $mail->addReplyTo($email, $full_name);
+//Set who the message is to be sent to
+    $mail->addAddress('garyws.items@gmail.com', 'gary tools');
+//Set the subject line
+    $mail->Subject = $email_subject;
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+    $mail->Body = $email_body;
+//send the message, check for errors
+    if (!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        header("location:email.php?status=thanks");
+    }//Create a new PHPMailer instance
 
-
+}
 $page_title = 'Tool Inquiry';
 $section = null;
 /**
@@ -71,19 +78,18 @@ $section = null;
 
 ?>
 <?php require(SHARED_PATH . '/header.php');
-      require(SHARED_PATH . '/nav.php');
-    ?>   <?php if(!empty($msg)) echo "<h2>$msg</h2>"; ?>
-<!--   --><?php //if (isset($_GET['status']) && $_GET['status'] == 'thanks') {  ?>
+require(SHARED_PATH . '/nav.php');
+?>
+<?php if (isset($_GET['status']) && $_GET['status'] == 'thanks') { ?>
     <div class="container text-center">
         <div class="row">
             <div class="col-12">
-
-<!--            <h2>Thank you for your interest in my tools!</h2>-->
-<!--                <p>I will try to be as prompt as possible in responding back to your questions or inquiry in purchase.</p>-->
+                <h2>Thank you for your interest in my tools!</h2>
+                <p>I will try to be as prompt as possible in responding back to your questions or inquiry in purchase.</p>
             </div>
         </div>
     </div>
-<!--    --><?php //} else { ?>
+<?php } else { ?>
     <section>
     <form action="email.php" method="POST" class="mt-3">
         <legend Class="text-center">Contact Form</legend>
@@ -98,9 +104,9 @@ $section = null;
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label ml-2" for="last-name">Last Name</label>
-               <div class="col-sm-8">
-                   <input type="text" class="form-control bar-width" id="last-name" name="last-name" placeholder="Smith">
-               </div>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control bar-width" id="last-name" name="last-name" placeholder="Smith">
+                </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label ml-2" for="email">Email</label>
@@ -125,30 +131,30 @@ $section = null;
         </fieldset>
         <legend class="text-center">Tool Inquiry</legend>
         <fieldset id="inquiry-info">
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label ml-2" for="subject">Subject:</label>
-                    <div class="col-sm-8">
-                        <input type="text" class="form-control bar-width" id="subject" name="subject" placeholder="subject or inquiry about tool.">
-                    </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label ml-2" for="subject">Subject:</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control bar-width" id="subject" name="subject" placeholder="subject or inquiry about tool.">
                 </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label ml-2" for="inquiry-questions">Questions Inquiries</label>
-                    <div class="col-sm-8">
-                        <textarea class="form-control bar-width" name="inquiry-questions" id="inquiry-questions" cols="30" rows="10" placeholder="Questions about an item or an offer on an item.."></textarea>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="bar-width col-sm-6">
-                        <button  class="btn btn-lg btn-outline-success btn-block b-width b-right"  type="submit">Submit</button>
-                    </div>
-                    <div class="bar-width col-sm-6">
-                        <button class="btn btn-lg btn-outline-danger btn-block b-width b-left" type="reset">Reset</button>
-                    </div>
-                </div>
-        </fieldset>
             </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label ml-2" for="inquiry-questions">Questions Inquiries</label>
+                <div class="col-sm-8">
+                    <textarea class="form-control bar-width" name="inquiry-questions" id="inquiry-questions" cols="30" rows="10" placeholder="Questions about an item or an offer on an item.."></textarea>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="bar-width col-sm-6">
+                    <button  class="btn btn-lg btn-outline-success btn-block b-width b-right"  type="submit">Submit</button>
+                </div>
+                <div class="bar-width col-sm-6">
+                    <button class="btn btn-lg btn-outline-danger btn-block b-width b-left" type="reset">Reset</button>
+                </div>
+            </div>
+        </fieldset>
+        </div>
         </div>
     </form>
-<!--     --><?php //} ?>
-   </section>
+<?php } ?>
+    </section>
 <?php include(SHARED_PATH . '/footer.php'); ?>
