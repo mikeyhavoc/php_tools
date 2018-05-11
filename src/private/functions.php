8af5430da88306dd3ddbd773df356da0682d02a5
michael williams <manicdesigner@gmail.com>
2018-05-10 23:34:45 -0400

    node_module function added.

--- src/private/functions.php
+++ src/private/functions.php
@@ -9,6 +9,10 @@ include 'initialize.php';
  */
 
 // script function to connect urls to root path.
+/**
+ * @param $script_path
+ * @return string
+ */
 function url_for($script_path) {
     // adds leading '/' if not present.
     if ($script_path[0] != '/') {
@@ -17,6 +21,19 @@ function url_for($script_path) {
 
     return WWW_ROOT . $script_path;
 }
+
+/**
+ * @param $node_modules
+ * @return string
+ */
+function node_module($node_modules) {
+    // leading ./ if not present.
+    if ($node_modules[0] != './') {
+        $node_modules = './' . $node_modules;
+    }
+    return $node_modules;
+}
+
 
 
 ################################################# queries to database,
