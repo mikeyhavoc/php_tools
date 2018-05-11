86b5c143f52a855853aba014742bbecbdadafb96
michael williams <manicdesigner@gmail.com>
2018-05-11 00:18:04 -0400

    analytics tracking file include.

---
+++ src/shared/analyticstracking.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikey
+ * Date: 5/10/18
+ * Time: 12:32 PM
+ */
+?>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116676832-2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-116676832-2');
+</script>
