ab9fe718334a308ee558110ae03537c91a274606
michael williams <manicdesigner@gmail.com>
2018-05-27 00:14:41 -0400

    mod file for footer to use cdns.

--- src/private/shared/footer.php
+++ src/private/shared/footer.php
@@ -4,9 +4,23 @@
     <p>Created by Michael Williams, &copy;<?php echo DATE(Y); ?>  last update <?php echo DATE("d, m, Y H:i:s.", getlastmod()); ?></p>
 
 </footer>
-<script  src="<?php echo node_module("node_modules/jquery/dist/jquery.min.js");?>"></script>
-<script  src="<?php echo url_for("node_modules/popper.js/dist/popper.js");?>"></script>
-<script  src="<?php echo url_for("node_modules/bootstrap/dist/js/bootstrap.min.js"); ?>"></script>
+
+<a title="Real Time Web Analytics" href="http://clicky.com/101120059"><img alt="Real Time Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script type="text/javascript">
+    var clicky_site_ids = clicky_site_ids || [];
+    clicky_site_ids.push(101120059);
+    (function() {
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = '//static.getclicky.com/js';
+        ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+    })();
+</script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384
 <script  src="<?php echo url_for( 'public/js/main.js');  ?>"></script>
 </body>
 </html>
