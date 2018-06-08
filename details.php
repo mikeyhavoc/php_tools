f838ac560c7376992b04c78f5934327994375bc0
mikey <manicdesigner@gmail.com>
2018-06-08 14:53:09 -0400

    removed html from main file. placed in a views file. learned from laracasts.com. requirements placed in bootstrapping file. GOAL reduce spaghetti code and make it cleaner, and more readable.
    Signed-off-by: mikey <manicdesigner@gmail.com>

--- details.php
+++ details.php
@@ -1,13 +1,12 @@
-
-
-<?php require_once('private/initialize.php');
-
+<?php
 /**
  * Created by PhpStorm.
- * User: mike
+ * User: mikey
  * Date: 12/21/17
- * Time: 11:52 PM
+ * Time: 11:02 AM
  */
+ require_once('bootstrap.php');
+
 $id_num = $_GET['id']; // grabbing Global Variable GET id
 $id = filter_var($id_num, FILTER_SANITIZE_NUMBER_INT); // filtering and sanitizing number going into array for value.
 $con = $db; // the connection to the db.
@@ -24,12 +23,12 @@ try {
                       INNER JOIN Images AS i ON i.t_id = t.t_id 
                       LEFT OUTER JOIN Types AS tt ON tt.tt_id = t.tt_id
                       WHERE t.t_id = :id LIMIT 1";
-$variables[':id'] = $id;
+    $variables[':id'] = $id;
 
     $items = execute_query($con, $single_item_full_query, $variables)->fetchAll();
 
 }catch(PDOException $e) {
-   echo $e->getMessage();
+    echo $e->getMessage();
 }
 try {
     $single_images_item_query = "SELECT t.t_id as id,
@@ -57,90 +56,6 @@ try {
 }catch(PDOException $e) {
     echo $e->getMessage();
 }
-$pageTitle = 'Details'; ?>
+$pageTitle = 'Details';
 
-<!doctype html>
-<html lang="en">
-<head>
-    <?php include SHARED_PATH . '/js/googleanalytics.js'; ?>
-    <?php include SHARED_PATH . '/js/hotjarAnalytics.js'; ?>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="keywords" content="garys tools, bradenton tools, bradenton gary tools, tool sale">
-    <meta name="description" content="Garys tools, retired bodyman selling his body tools locally in bradenton fl.">
-    <link href="https://fonts.googleapis.com/css?family=Supermercado+One" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo node_module('public/css/main.css'); ?>">
-
-    <title><?php echo $page_title; ?></title>
-</head>
-<body>
-<header role="banner" class='container-fluid'>
-    <section class='row'>
-        <h1 class="col-12 col-md-4 tools logo__section logo">
-            <a class="logo__title logo" href="<?php echo url_for( 'index.php'); ?>">Garys Tools</a>
-        </h1>
-    </section>
-</header>
-
-<?php include(SHARED_PATH . '/nav.php'); ?>
-
-
-
-<div class="row">
-    <div class="col-12">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>">Home</a></li>
-                <?php if(isset($breadcrumb)) { ?>
-                <?php foreach ($breadcrumb as $crumb) { ?>
-                    <li class="breadcrumb-item"><a href="catalog.php?cat=<?php echo $crumb['category']; ?>"><?php echo $crumb['category']; ?></a></li>
-                    <li class="breadcrumb-item active"><?php echo $crumb['code']; ?></li>
-                <?php } ?>
-                <?php } /* isset for breadcrumb*/?>
-            </ol>
-        </nav>
-    </div><!--/col-12-->
-</div><!-- /.row -->
-
-
-<section>
-    <div class="container">
-        <div class="row">
-                <?php if(isset($items)) {   ?>
-                <?php foreach ( $items as $item) { ?>
-                    <div class="col-12 col-sm-6">
-                        <article role="article" class='card'>
-                            <h1>Code: <?php echo $item['code']; ?></h1>
-                            <h3>Name: <?php echo $item['name']; ?></h3>
-                            <h3>Brand:<?php echo $item['brand']; ?></h3>
-                            <h4>Category: <?php echo $item['category']; ?></h4>
-                            <h4>Pieces: <?php echo $item['pieces']; ?></h4>
-                            <h4>Quantity: <?php echo $item['quantity']; ?></h4>
-                            <h4>Retail Price $ <?php echo $retail = ($item['retail'] == 0 ? ' N/A' : $item['retail']); ?></h4>
-                            <h4>Price: <?php echo $price = ($item['price'] == 0 ? 'Make offer' : '$' . $item['price']); ?></h4>
-                            <h4 class="sale">Sold: <?php echo  $sold = ($item['sold'] == 0 ? 'For Sale' : 'sold'); ?></h4>
-                            <h4>Code: <?php echo $item['code']; ?></h4>
-                            <p>Description: <?php echo $item['description']; ?></p>
-
-                        </article>
-                    </div>
-
-                <?php } ?>
-            <?php } /* isset for $items */?>
-                <?php if(isset($images)) { ?>
-                <?php  foreach ($images as $image) { ?>
-                    <div class="col-12 col-sm-6">
-                        <img class='card' src='<?php echo IMAGES . $image['image']; ?>' alt='<?php echo $image['description']; ?>'>
-                    </div>
-                <?php  } ?>
-            <?php } /* images isset */?>
-
-    </div>
-<section/>
-        <?php include (SHARED_PATH . 'js/quckyAnalaytids.html'); ?>
-<?php
-require(SHARED_PATH . '/footer.php');
-?>
+require_once('views/details.view.php');
