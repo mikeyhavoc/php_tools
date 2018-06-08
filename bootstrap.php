55a4ea0c23211cf595b5e1217093f2cf0037699f
mikey <manicdesigner@gmail.com>
2018-06-08 14:45:38 -0400

    add require_once for initialize, functions, and connection.

--- bootstrap.php
+++ bootstrap.php
@@ -5,3 +5,10 @@
  * Date: 6/8/18
  * Time: 2:17 PM
  */
+
+require_once 'private/initialize.php';
+require_once 'private/functions.php';
+require_once 'Database/Connection.php';
+
+
+$db = Connection::make();
