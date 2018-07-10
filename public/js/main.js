9d2ba42b7a0f3faf7be41b2fbf356dd4b541e1ec
mikey <manicdesigner@gmail.com>
2018-07-10 16:52:08 -0400

    nothing really changed.

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,12 @@
 //     return false;
 // });
 
-$(document).ready(function) {
 
 
-    $('a[href=#top]').click(function () {
+
+    $('#top').click(function () {
         $("html, body").animate({scrollTop: 0}, 'slow');
         return false;
     });
 
-});
+
